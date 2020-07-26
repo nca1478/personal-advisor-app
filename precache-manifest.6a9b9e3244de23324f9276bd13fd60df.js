37694ae7dd8d7d743f438c3317c33c3ec4abea1f
Nelson Cadenas <nca1478@yahoo.com>
2020-07-26 01:48:06 -0400

    Updates

---
+++ precache-manifest.6a9b9e3244de23324f9276bd13fd60df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89268661f4d58ded82273201a79d60af",
+    "url": "/personal-advisor-app/index.html"
+  },
+  {
+    "revision": "f863d3bb64de491575ba",
+    "url": "/personal-advisor-app/static/css/main.6565bf5d.chunk.css"
+  },
+  {
+    "revision": "49748f4372eb5914e6b8",
+    "url": "/personal-advisor-app/static/js/2.1938e8f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-advisor-app/static/js/2.1938e8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f863d3bb64de491575ba",
+    "url": "/personal-advisor-app/static/js/main.e8ccd93a.chunk.js"
+  },
+  {
+    "revision": "a2a758703e3548d5f139",
+    "url": "/personal-advisor-app/static/js/runtime-main.852fcb22.js"
+  }
+]);
